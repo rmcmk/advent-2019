9f049a942918794a705a656b2345eee675a8767a
rmcmk <ryley.kimmel@live.com>
2019-12-10 00:29:48 -0500

    Day 4 part 2

--- src/main/kotlin/me/ryleykimmel/advent/day4/Day4.kt
+++ src/main/kotlin/me/ryleykimmel/advent/day4/Day4.kt
@@ -11,12 +11,20 @@ fun parsePasswordRange(name: String): IntRange {
 
 fun String.`is ascending`() = this.split("").sorted().joinToString("") { it } == this
 fun String.`has any adjacent pair`() = this.filterIndexed { index, s -> s == this.elementAtOrNull(index + 1)}.isNotEmpty()
+fun String.`has exactly one adjacent pair`() = this.groupBy { it }.any { it.value.size == 2 }
 
 fun part1(range: IntRange) = range.filter {
     val password = it.toString()
     password.`is ascending`() && password.`has any adjacent pair`()
 }.size
 
+fun part2(range: IntRange) = range.filter {
+    val password = it.toString()
+    password.`is ascending`() && password.`has exactly one adjacent pair`()
+}.size
+
 fun main() {
-    println("p1: ${part1(parsePasswordRange("day4"))}")
+    val range = parsePasswordRange("day4")
+    println("p1: ${part1(range)}")
+    println("p2: ${part2(range)}")
 }
